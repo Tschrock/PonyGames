26149e476929150958a4371bc2b88a78236959b2
CyberPon3 <tschrock123@gmail.com>
2018-01-27 17:58:57 -0500

    Update test data generation

--- testData.ts
+++ testData.ts
@@ -13,21 +13,12 @@ import { Tag } from './models/Tag';
 import { ProjectTag } from './models/ProjectTag';
 
 
-function createTag(tag: string | string[]): PromiseLike<Tag | Tag[]> {
-    if (Array.isArray(tag)) {
-        return Promise.all(tag.map(t => Tag.create({ key: t })));
-    }
-    else {
-        return Tag.create({ key: tag });
-    }
+function getRandomTags(tagGroups: Array<Array<Tag>>) {
+    return _.sampleSize(tagGroups, _.random(2, Math.floor(tagGroups.length * 0.7))).map(t => _.sample(t)) as Array<Tag>;
 }
 
-function getRandomTags(tagGroups: Array<Tag | Array<Tag>>) {
-    return _.sampleSize(tagGroups, _.random(2, Math.floor(tagGroups.length * 0.7))).map(t => Array.isArray(t) ? _.sample(t) : t) as Array<Tag>;
-}
-
-function createArr<T>(count: number, builderFunc: (index: number) => T): Array<T> {
-    return new Array(count).fill('').map((_, i) => builderFunc(i));
+function createArr<T>(count: number, builderFunc: (index: number) => PromiseLike<T>) {
+    return Promise.all(new Array(count).fill(0).map((_, i) => builderFunc(i)));
 }
 
 
@@ -36,27 +27,42 @@ export async function loadTestData() {
 
         const tagGroups = await Promise.all([
             ['2D', '2.5D', '3D'],
-            'Online',
-            'MMO',
-            'Multiplayer',
-            'Visual Novel',
-            'Windows',
-            'Mac',
-            'Linux',
-            'Kickstarted',
-            'Open Source',
-            'Steam Greenlit',
+            ['Online'],
+            ['MMO'],
+            ['Multiplayer'],
+            ['Visual Novel'],
+            ['Windows'],
+            ['Mac'],
+            ['Linux'],
+            ['Kickstarted'],
+            ['Open Source'],
+            ['Steam Greenlit'],
             ['Wine (Platnum)', 'Wine (Silver)', 'Wine (Bronse)', 'Wine (Garbage)'],
-            'Side-Scrolling',
-            'Keyboard and Mouse',
-            'Gamepad',
-            'Touchscreen',
-            'Pixel'
-        ].map(s => createTag(s)));
+            ['Side-Scrolling'],
+            ['Keyboard and Mouse'],
+            ['Gamepad'],
+            ['Touchscreen'],
+            ['Pixel']
+        ].map(s => Promise.all(s.map(t => Tag.create({ key: t })))));
 
-        const teams = await Promise.all(createArr(20, t => Team.create({ name: faker.company.companyName() })));
-        const projects = await Promise.all(new Array(20).fill(0).map((t, i) => Project.create({ name: faker.commerce.productName(), shortDescription: faker.company.catchPhrase(), description: faker.lorem.paragraph(6), teamId: teams[i].id })));
-        const projectTags = await Promise.all(projects.map(p => Promise.all(getRandomTags(tagGroups).map(t => ProjectTag.create({ projectId: p.id, tagId: t.id })))));
+        const teams = await createArr(20, t => Team.create({
+            name: faker.company.companyName()
+        }));
+
+        const projects = await createArr(teams.length, i => Project.create({
+            name: faker.commerce.productName(),
+            shortDescription: faker.company.catchPhrase(),
+            description: faker.lorem.paragraph(6),
+            teamId: teams[i].id
+        }));
+
+        const projectTags2 = await createArr(projects.length, i => Promise.all(
+            getRandomTags(tagGroups)
+            .map(t => ProjectTag.create({
+                projectId: projects[i].id,
+                tagId: t.id
+            }))
+        ));
 
     });
 }
