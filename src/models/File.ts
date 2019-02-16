93b2f4cbf66ad9823ae8d8b48acc0372ec550d72
CyberPon3 <tschrock123@gmail.com>
2019-02-16 08:37:49 -0500

    Update file model

--- src/models/File.ts
+++ src/models/File.ts
@@ -6,6 +6,8 @@
 
 import { Sequelize, Table, Column, Model, HasMany, BelongsToMany, ForeignKey, BelongsTo } from 'sequelize-typescript';
 
+import { FileUploadStatus } from 'src/lib/FileUploadStatus';
+
 import { Tag } from './Tag';
 import { FileTag } from './FileTag';
 import { FileHash } from './FileHash';
@@ -20,25 +22,41 @@ import { FileSource } from './FileSource';
 })
 export class File extends Model<File> {
 
-    /** A descriptive title for the file. */
-    @Column
-    public title!: string;
+    // General
 
     /** The filename. */
-    @Column
+    @Column(Sequelize.STRING(255))
     public filename!: string;
 
+    /** The mimetype. */
+    @Column(Sequelize.STRING(255))
+    public mimetype!: string;
+
+    /** The size (in bytes). */
+    @Column(Sequelize.INTEGER)
+    public size!: number;
+
+    // Display
+
+    /** A title for the file. */
+    @Column(Sequelize.STRING(255))
+    public title!: string;
+
     /** The version of the file. */
-    @Column
+    @Column(Sequelize.STRING(255))
     public version!: string;
 
     /** The version order of the file (Because some people are derp and change versioning weirdly). */
-    @Column
+    @Column(Sequelize.INTEGER)
     public versionOrder!: number;
 
-    /** The File's Tags. */
-    @BelongsToMany(() => Tag, () => FileTag)
-    public tags!: Tag[];
+    /** Whether or not the File is available for download. */
+    @Column
+    public isAvailable!: boolean;
+
+    /** A message describing why the File is unavailable. */
+    @Column(Sequelize.TEXT)
+    public unavailableMessage!: string;
 
     /** The Hashes for this File. */
     @HasMany(() => FileHash)
@@ -48,13 +66,68 @@ export class File extends Model<File> {
     @HasMany(() => FileSource)
     public fileSources!: FileSource[];
 
-    /** Whether or not the File is available for download. */
-    @Column
-    public isAvailable!: boolean;
+    // General Upload
 
-    /** A message describing why the File is unavailable. */
+    /** The upload status. */
+    @Column(Sequelize.INTEGER)
+    public uploadstatus!: FileUploadStatus;
+
+    /** The client upload token. */
+    @Column(Sequelize.STRING(255))
+    public clientuploadtoken!: string;
+
+    /** The time this File started being uploaded. */
+    @Column
+    public upload_started_at!: Date
+
+    /** The time this File finished being uploaded. */
+    @Column
+    public upload_finished_at!: Date
+
+    // B2 Upload
+
+    /** The upload url. */
     @Column(Sequelize.TEXT)
-    public unavailableMessage!: string;
+    public b2uploadparturl!: string;
+
+    /** The upload token. */
+    @Column(Sequelize.TEXT)
+    public b2uploadtoken!: string;
+
+    /** The time the upload url will expire. */
+    @Column(Sequelize.TEXT)
+    public b2uploadurl_expires_at!: Date;
+
+    // General Storage
+
+    /** The storage url. */
+    @Column(Sequelize.TEXT)
+    public url!: string;
+
+    // B2 Storage
+
+    /** The storage account id. */
+    @Column(Sequelize.TEXT)
+    public b2accountid!: string;
+
+    /** The storage bucket id. */
+    @Column(Sequelize.TEXT)
+    public b2bucketid!: string;
+
+    /** The storage file id. */
+    @Column(Sequelize.TEXT)
+    public b2fileid!: string;
+
+    /** The storage file name. */
+    @Column(Sequelize.TEXT)
+    public b2filename!: string;
+
+
+    // Organization & Search
+
+    /** The File's Tags. */
+    @BelongsToMany(() => Tag, () => FileTag)
+    public tags!: Tag[];
 
     /** The Id of the File's Group. */
     @ForeignKey(() => FileGroup)
